d896a25c83244b44da1afd5dd3d136b871f50477
lenahitraya101-alt <lenahitraya101@gmail.com>
2026-06-30 00:15:06 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+const trackTitle = document.getElementById("track-title");
+const artistName = document.getElementById("artist-name");
+
+trackTitle.textContent = "Golden Brown";
+artistName.textContent = "The Stranglers";
