6ad0527361dda21505c048c702a0966ca5c533ec
lenahitraya101-alt <lenahitraya101@gmail.com>
2026-06-30 00:47:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const trackTitle = document.getElementById("track-title");
-const artistName = document.getElementById("artist-name");
+const API_KEY = "AIzaSyCt3UaI_g5o3alle4cG1de93Kf3w_DMGzQ";
+const PLAYLIST_ID = "PLFAHF2YV2uz0&si=PM5rL4P3caG01Vnz";
+const MAX_RESULTS = 50;
+async function loadPlaylist() {
+  const url =
+    `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=${MAX_RESULTS}&playlistId=${PLAYLIST_ID}&key=${API_KEY}`;
 
-trackTitle.textContent = "Golden Brown";
-artistName.textContent = "The Stranglers";
+  const response = await fetch(url);
+  const data = await response.json();
+
+  console.log(data);
+}
+
+loadPlaylist();
